b5314a6e0edf4fbf23dd0c359085c2be7fb18ac9
Carlos Adriano <cadrianogamac@gmail.com>
2022-08-24 11:41:53 -0300

    Create part of the colapsed sidebar logic

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleButton = document.querySelector(".toggle-sidebar-button");
+const sidebar = document.querySelector("#sidebar")
+const content = document.querySelector("#content");
+const itemNames = document.querySelectorAll(".item-name");
+
+toggleButton.addEventListener('click', (e) => {
+  itemNames.forEach(item => item.classList.toggle("show-item-name"));
+  sidebar.classList.toggle("colapsed");
+  content.classList.toggle("transitioned");
+});
