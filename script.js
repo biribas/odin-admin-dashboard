edbbf8be20a7232a9d255d366b4a2c6fe5bd803d
Carlos Adriano <cadrianogamac@gmail.com>
2022-08-25 13:33:48 -0300

    Create template of the mobile buttons event listeners

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ const toggleIcon = document.querySelector(".toggle-icon");
 const sidebar = document.querySelector("#sidebar")
 const content = document.querySelector("#content");
 
+const hamburguer = document.querySelector(".hamburguer");
+const announcementsButton = document.querySelector("announcements-button")
+const trendingButton = document.querySelector("trending-button")
+
 toggleButton.addEventListener('click', (e) => {
   toggleButton.classList.toggle("collapsed");
   toggleIcon.classList.toggle("rotated");
   sidebar.classList.toggle("collapsed");
   sidebar.classList.toggle("hidden");
   content.classList.toggle("collapsed");
+});
+
+hamburguer.addEventListener('click', e => {
+
+});
+
+announcementsButton.addEventListener('click', e => {
+
+});
+
+trendingButton.addEventListener('click', e => {
+
 });
