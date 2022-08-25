a709fd0fbde0cf45cd8667dbb72f0c6cda770fa3
Carlos Adriano <cadrianogamac@gmail.com>
2022-08-25 17:19:02 -0300

    Silly changes on the sidebar resposivity

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const sidebar = document.querySelector("#sidebar")
 const content = document.querySelector("#content");
 
 const hamburguer = document.querySelector(".hamburguer");
-const announcementsButton = document.querySelector("announcements-button")
-const trendingButton = document.querySelector("trending-button")
+const announcementsButton = document.querySelector(".announcements-button")
+const trendingButton = document.querySelector(".trending-button")
+
+const hideSidebarQuery = window.matchMedia("(max-width: 750px)");
 
 toggleButton.addEventListener('click', (e) => {
-  toggleButton.classList.toggle("collapsed");
+  document.body.classList.toggle("collapsed");
+  document.body.classList.toggle("hidden");
   toggleIcon.classList.toggle("rotated");
-  sidebar.classList.toggle("collapsed");
-  sidebar.classList.toggle("hidden");
-  content.classList.toggle("collapsed");
 });
 
 hamburguer.addEventListener('click', e => {
-
+  
 });
 
 announcementsButton.addEventListener('click', e => {
@@ -26,3 +26,14 @@ announcementsButton.addEventListener('click', e => {
 trendingButton.addEventListener('click', e => {
 
 });
+
+hideSidebarQuery.onchange = e => {
+  if (e.matches) {
+    document.body.classList.add("collapsed", "hidden");
+    toggleIcon.classList.remove("rotated");
+    sidebar.classList.add("hidden");
+  }
+  else {
+    sidebar.classList.remove("hidden");
+  }
+}
