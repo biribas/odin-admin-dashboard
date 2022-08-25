ee994a14abc449a8718b057bd50c947a37e97b4a
Carlos Adriano <cadrianogamac@gmail.com>
2022-08-24 23:05:27 -0300

    Fix bugs in sidebar and improve code readability

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const toggleButton = document.querySelector(".toggle-sidebar-button");
+const toggleIcon = document.querySelector(".toggle-icon");
 const sidebar = document.querySelector("#sidebar")
 const content = document.querySelector("#content");
-const itemNames = document.querySelectorAll(".item-name");
 
 toggleButton.addEventListener('click', (e) => {
-  itemNames.forEach(item => item.classList.toggle("show-item-name"));
-  toggleButton.classList.toggle("rotate");
-  sidebar.classList.toggle("colapsed");
-  content.classList.toggle("transitioned");
+  toggleButton.classList.toggle("collapsed");
+  toggleIcon.classList.toggle("rotated");
+  sidebar.classList.toggle("collapsed");
+  sidebar.classList.toggle("hidden");
+  content.classList.toggle("collapsed");
 });
