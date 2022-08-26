09f8532f587761f20fbd1f9319ef211ff0e4e2e1
Carlos Adriano <cadrianogamac@gmail.com>
2022-08-26 11:59:39 -0300

    Add overlay in mobile version

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const toggleIcon = document.querySelector(".toggle-icon");
 const sidebar = document.querySelector("#sidebar")
 const announcements = document.querySelector("#announcements");
 const trending = document.querySelector("#trending");
+const overlay = document.querySelector(".overlay");
 
 const hamburguer = document.querySelector(".hamburguer");
 const announcementsButton = document.querySelector(".announcements-button")
@@ -10,23 +11,34 @@ const trendingButton = document.querySelector(".trending-button")
 
 const hideSidebarQuery = window.matchMedia("(max-width: 750px)");
 
-toggleButton.addEventListener('click', (e) => {
+toggleButton.addEventListener('click', () => {
   document.body.classList.toggle("collapsed");
   sidebar.classList.toggle("hidden-names");
   toggleIcon.classList.toggle("rotated");
 });
 
-hamburguer.addEventListener('click', e => {
+hamburguer.addEventListener('click', () => {
   document.body.classList.remove("collapsed");
   sidebar.classList.remove("hidden-names");
+  overlay.classList.remove("hidden");
 });
 
-announcementsButton.addEventListener('click', e => {
+announcementsButton.addEventListener('click', () => {
   announcements.classList.add("show");
+  overlay.classList.remove("hidden");
 });
 
-trendingButton.addEventListener('click', e => {
+trendingButton.addEventListener('click', () => {
   trending.classList.add("show");
+  overlay.classList.remove("hidden");
+});
+
+overlay.addEventListener('click', () => {
+  overlay.classList.add('hidden');
+  announcements.classList.remove("show");
+  trending.classList.remove("show");
+  document.body.classList.add("collapsed");
+  sidebar.classList.add("hidden-names");
 });
 
 hideSidebarQuery.onchange = e => {
